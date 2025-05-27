1fec8fab393f8f785de1db896dc8698bad2ca9b7
Jani Monoses <jani.monoses@gmail.com>
2025-05-27 15:20:55 +0300

    Add llm example app

---
+++ examples/llm/gpt2.rs
@@ -0,0 +1,213 @@
+// GPT-2 model description
+
+use super::{Config, ModelBuilder};
+use catgrad::backend::cpu::eval::{Builder, EvalState};
+use catgrad::backend::cpu::ndarray::{NdArray, TaggedNdArray};
+use catgrad::core::nn::layers::*;
+use catgrad::core::{Dtype, NdArrayType, Shape, Var};
+use std::collections::HashMap;
+
+pub struct Model;
+
+impl Model {
+    // The original GPT2 checkpoints use a Conv1D layer instead of linear,
+    // equivalent to a linear layer with weights in transposed order
+    fn gpt_linear(builder: &Builder, in_dim: usize, out_dim: usize, name: &str, x: Var) -> Var {
+        let w_type = NdArrayType {
+            shape: Shape(vec![in_dim, out_dim]),
+            dtype: x.label.dtype,
+        };
+        // Bias
+        let b_type = NdArrayType {
+            shape: Shape(vec![out_dim]),
+            dtype: x.label.dtype,
+        };
+
+        let w = parameter(builder, w_type.clone(), format!("{name}.weight"));
+        let b = parameter(builder, b_type.clone(), format!("{name}.bias"));
+
+        // w is already transposed in GPT-2 checkpoints
+        let mut w_t = w;
+
+        if x.label.shape.0.len() == 3 {
+            let batch_size = x.label.shape.0[0];
+            w_t = expand(builder, Shape(vec![batch_size, in_dim, out_dim]), w_t);
+        }
+
+        let m = mat_mul(builder, x, w_t);
+        let bb = expand(builder, m.label.shape.clone(), b);
+        m + bb
+    }
+
+    pub fn embeddings(builder: &Builder, config: &Config, x: Var) -> Var {
+        let t = NdArrayType {
+            shape: Shape(vec![config.vocab_size, config.n_embd]),
+            dtype: Dtype::F32,
+        };
+        let weights = parameter(builder, t, format!("wte.weight"));
+        let we = embedding(builder, x.clone(), weights);
+
+        let t = NdArrayType {
+            shape: Shape(vec![config.n_positions, config.n_embd]),
+            dtype: Dtype::F32,
+        };
+        let pos = arange(&builder, x.label.clone());
+        let weights = parameter(builder, t, format!("wpe.weight"));
+        let pe = embedding(builder, pos, weights);
+
+        we + pe
+    }
+
+    pub fn attention(builder: &Builder, config: &Config, name: &str, x: Var) -> Var {
+        let dim = config.n_embd;
+        let num_heads = config.n_head;
+        let head_dim = dim / num_heads;
+
+        let b = x.clone().label.shape.0[0];
+        let s = x.clone().label.shape.0[1];
+
+        // let c_attn = gpt_linear(builder, dim, 3 * dim, &format!("{name}.c_attn"), x.clone());
+        let k = Model::gpt_linear(builder, dim, dim, &format!("{name}.key"), x.clone());
+        let q = Model::gpt_linear(builder, dim, dim, &format!("{name}.query"), x.clone());
+        let v = Model::gpt_linear(builder, dim, dim, &format!("{name}.value"), x.clone());
+
+        let q = reshape(builder, Shape(vec![b, s, num_heads, head_dim]), q);
+        let k = reshape(builder, Shape(vec![b, s, num_heads, head_dim]), k);
+        let v = reshape(builder, Shape(vec![b, s, num_heads, head_dim]), v);
+
+        let q = transpose(builder, 1, 2, q);
+        let k = transpose(builder, 1, 2, k);
+        let v = transpose(builder, 1, 2, v);
+
+        let tk = transpose(builder, 2, 3, k);
+        let attn = mat_mul(builder, q.clone(), tk);
+        let denom = constant(builder, attn.label.clone(), f32::sqrt(head_dim as f32));
+        let attn = attn / denom;
+
+        let mask = causal_mask(builder, s);
+        let mask = expand(builder, Shape(vec![b, num_heads, s, s]), mask);
+        let attn = attn + mask;
+
+        let attn = softmax(builder, attn);
+        let attn = mat_mul(builder, attn, v);
+
+        let attn = transpose(builder, 1, 2, attn);
+        let attn = reshape(builder, Shape(vec![b, s, dim]), attn);
+
+        let c_proj = Model::gpt_linear(builder, dim, dim, &format!("{name}.c_proj"), attn);
+        c_proj
+    }
+
+    pub fn mlp(builder: &Builder, dim: usize, name: &str, x: Var) -> Var {
+        let x = Model::gpt_linear(builder, dim, dim * 4, &format!("{name}.c_fc"), x);
+        let x = gelu(builder, x);
+        let x = Model::gpt_linear(builder, dim * 4, dim, &format!("{name}.c_proj"), x);
+        x
+    }
+
+    pub fn layer(builder: &Builder, config: &Config, name: &str, x: Var) -> Var {
+        let res = x.clone();
+        let x = layernorm(
+            &builder,
+            config.layer_norm_epsilon,
+            &format!("{name}.ln_1"),
+            x,
+        );
+        let x = Model::attention(builder, config, &format!("{name}.attn"), x);
+        let x = res + x;
+        let res = x.clone();
+        let x = layernorm(
+            &builder,
+            config.layer_norm_epsilon,
+            &format!("{name}.ln_2"),
+            x,
+        );
+        let x = Model::mlp(builder, config.n_embd, &format!("{name}.mlp"), x);
+        x + res
+    }
+}
+
+impl ModelBuilder for Model {
+    fn build(&mut self, batches: usize, tokens: usize, config: &Config) -> EvalState {
+        let in_type = NdArrayType {
+            shape: Shape(vec![batches, tokens]),
+            dtype: Dtype::I32,
+        };
+
+        let state = EvalState::build(|builder| {
+            let x = Var::new(builder.clone(), in_type.clone());
+            let emb = Model::embeddings(&builder, config, x.clone());
+
+            let mut result = emb;
+
+            for i in 0..config.n_layer {
+                result = Model::layer(&builder, &config, &format!("h.{i}"), result);
+            }
+
+            result = layernorm(
+                &builder,
+                config.layer_norm_epsilon,
+                &format!("ln_f"),
+                result,
+            );
+
+            // GPT-2 uses weight tying so lm_head is the same as wte
+            let lm_head = linear_no_bias(&builder, config.n_embd, config.vocab_size, "wte", result);
+            (vec![x], vec![lm_head])
+        });
+
+        state
+    }
+
+    fn post_load(&mut self, tensors: &mut HashMap<String, TaggedNdArray>) {
+        let attn_keys: Vec<String> = tensors
+            .keys()
+            .filter(|k| k.contains("c_attn"))
+            .cloned()
+            .collect();
+
+        // Split these keys into component Q, K, and V
+        for attn_key in attn_keys {
+            if let Some(TaggedNdArray::F32(array)) = tensors.get(&attn_key) {
+                let mut shape = array.shape.clone();
+                let l = shape.0.len() - 1;
+                let dim = shape.0[l] / 3;
+
+                // Create the three tensors
+                let q_name = attn_key.replace("c_attn", "query");
+                let k_name = attn_key.replace("c_attn", "key");
+                let v_name = attn_key.replace("c_attn", "value");
+
+                let m = shape.size() / shape.0[l];
+
+                // Split the tensor data
+                let mut q_data: Vec<f32> = Vec::with_capacity(m * dim);
+                let mut k_data: Vec<f32> = Vec::with_capacity(m * dim);
+                let mut v_data: Vec<f32> = Vec::with_capacity(m * dim);
+                for (i, c) in array.data.chunks_exact(dim).enumerate() {
+                    if i % 3 == 0 {
+                        q_data.extend_from_slice(c);
+                    } else if i % 3 == 1 {
+                        k_data.extend_from_slice(c);
+                    } else {
+                        v_data.extend_from_slice(c);
+                    }
+                }
+
+                shape.0[l] = dim;
+
+                // Create new arrays with proper shapes
+                let q_shape = shape.clone();
+                let k_shape = shape.clone();
+                let v_shape = shape;
+
+                // Insert the new tensors
+                tensors.insert(q_name, TaggedNdArray::F32(NdArray::new(q_data, q_shape)));
+                tensors.insert(k_name, TaggedNdArray::F32(NdArray::new(k_data, k_shape)));
+                tensors.insert(v_name, TaggedNdArray::F32(NdArray::new(v_data, v_shape)));
+
+                tensors.remove(&attn_key);
+            }
+        }
+    }
+}
---
+++ examples/llm/olmo.rs
@@ -0,0 +1,162 @@
+// OLMo-2 model description
+
+use super::{Config, ModelBuilder};
+use catgrad::backend::cpu::eval::{Builder, EvalState};
+use catgrad::core::nn::layers::*;
+use catgrad::core::{Dtype, NdArrayType, Shape, Var};
+
+pub struct Model;
+
+impl ModelBuilder for Model {
+    fn build(&mut self, batches: usize, tokens: usize, config: &Config) -> EvalState {
+        let in_type = NdArrayType {
+            shape: Shape(vec![batches, tokens]),
+            dtype: Dtype::I32,
+        };
+
+        let state = EvalState::build(|builder| {
+            let x = Var::new(builder.clone(), in_type.clone());
+            let emb = Model::embeddings(builder, config, x.clone());
+            let mut result = emb;
+
+            for i in 0..1 {
+                // for i in 0..config.num_hidden_layers {
+                result = Model::layer(builder, config, &format!("model.layers.{i}"), result);
+            }
+
+            result = rmsnorm(builder, config.rms_norm_eps, &format!("model.norm"), result);
+            let lm_head = linear_no_bias(
+                builder,
+                config.hidden_size,
+                config.vocab_size,
+                "lm_head",
+                result,
+            );
+
+            (vec![x], vec![lm_head])
+        });
+        state
+    }
+}
+
+impl Model {
+    pub fn embeddings(builder: &Builder, config: &Config, x: Var) -> Var {
+        let t = NdArrayType {
+            shape: Shape(vec![config.vocab_size, config.hidden_size]),
+            dtype: Dtype::F32,
+        };
+        let weights = parameter(builder, t, format!("model.embed_tokens.weight"));
+        embedding(builder, x.clone(), weights)
+    }
+
+    pub fn attention(builder: &Builder, config: &Config, name: &str, x: Var) -> Var {
+        let dim = config.hidden_size;
+        let num_heads = config.num_attention_heads;
+        let num_kv_heads = config.num_key_value_heads;
+        let head_dim = config.hidden_size / num_heads;
+        let b = x.clone().label.shape.0[0];
+        let s = x.clone().label.shape.0[1];
+
+        let q = linear_no_bias(builder, dim, dim, &format!("{name}.q_proj"), x.clone());
+        let k = linear_no_bias(
+            builder,
+            dim,
+            dim * num_kv_heads / num_heads,
+            &format!("{name}.k_proj"),
+            x.clone(),
+        );
+        let v = linear_no_bias(
+            builder,
+            dim,
+            dim * num_kv_heads / num_heads,
+            &format!("{name}.v_proj"),
+            x.clone(),
+        );
+
+        let q = rmsnorm(builder, config.rms_norm_eps, &format!("{name}.q_norm"), q);
+        let k = rmsnorm(builder, config.rms_norm_eps, &format!("{name}.k_norm"), k);
+
+        let q = reshape(builder, Shape(vec![b, s, num_heads, head_dim]), q);
+        let k = reshape(builder, Shape(vec![b, s, num_kv_heads, head_dim]), k);
+        let v = reshape(builder, Shape(vec![b, s, num_kv_heads, head_dim]), v);
+
+        let q = transpose(builder, 1, 2, q);
+        let k = transpose(builder, 1, 2, k);
+        let v = transpose(builder, 1, 2, v);
+
+        // TODO KV cache
+        // let max_seq_len = 10;
+        // let m = pad_mask(builder, max_seq_len, k.label.shape.0[3]);
+        // let mut kv_shape = k.label.shape.clone();
+        // kv_shape.0[2] = max_seq_len;
+        // let ke = expand(builder, kv_shape.clone(), k.clone());
+        // let m = expand(builder, kv_shape, m);
+        // print(builder, "KE", false, &ke);
+        // print(builder, "M", true, &m);
+        // let res = ke * m;
+
+        let tk = transpose(builder, 2, 3, k);
+        let attn = mat_mul(builder, q.clone(), tk);
+        let denom = constant(builder, attn.label.clone(), f32::sqrt(head_dim as f32));
+        let attn = attn / denom;
+
+        let mask = causal_mask(builder, s);
+        let mask = expand(builder, Shape(vec![b, num_heads, s, s]), mask);
+        let attn = attn + mask;
+
+        let attn = softmax(builder, attn);
+        let attn = mat_mul(builder, attn, v);
+        let x = transpose(builder, 1, 2, attn);
+        let x = reshape(builder, Shape(vec![b, s, dim]), x);
+        let o_proj = linear_no_bias(builder, dim, dim, &format!("{name}.o_proj"), x);
+        o_proj
+    }
+
+    pub fn mlp(builder: &Builder, config: &Config, name: &str, x: Var) -> Var {
+        let gated = linear_no_bias(
+            builder,
+            config.hidden_size,
+            config.intermediate_size,
+            &format!("{name}.gate_proj"),
+            x.clone(),
+        );
+        let up = linear_no_bias(
+            builder,
+            config.hidden_size,
+            config.intermediate_size,
+            &format!("{name}.up_proj"),
+            x,
+        );
+        let x = silu(builder, gated) * up; // SwiGLU
+        let x = linear_no_bias(
+            builder,
+            config.intermediate_size,
+            config.hidden_size,
+            &format!("{name}.down_proj"),
+            x,
+        );
+        x
+    }
+
+    pub fn layer(builder: &Builder, config: &Config, name: &str, x: Var) -> Var {
+        let res = x.clone();
+        let x = Model::attention(builder, config, &format!("{name}.self_attn"), x);
+        let x = rmsnorm(
+            &builder,
+            config.rms_norm_eps,
+            &format!("{name}.post_attention_layernorm"),
+            x,
+        );
+        let x = res + x;
+
+        let res = x.clone();
+        let x = Model::mlp(builder, config, &format!("{name}.mlp"), x);
+        let x = rmsnorm(
+            &builder,
+            config.rms_norm_eps,
+            &format!("{name}.post_feedforward_layernorm"),
+            x,
+        );
+        x + res
+    }
+}
