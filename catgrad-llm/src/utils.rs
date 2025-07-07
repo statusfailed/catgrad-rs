56273e0f3361033a036044cb1b07c271535a70a3
Jani Monoses <jani.monoses@gmail.com>
2025-07-07 14:29:22 +0300

    Move method to utils

--- catgrad-llm/src/utils.rs
+++ catgrad-llm/src/utils.rs
@@ -1,5 +1,6 @@
 use catgrad::backend::cpu::ndarray::{NdArray, TaggedNdArray};
 use catgrad::core::Shape;
+use hf_hub::api::sync::Api;
 use std::collections::HashMap;
 use std::path::PathBuf;
 
@@ -61,4 +62,31 @@ pub fn read_safetensors_multiple(path: Vec<PathBuf>) -> HashMap<String, TaggedNd
         read_safetensors_file(path, &mut map);
     }
     map
+}
+
+pub fn get_model_files(model: &str) -> (Vec<PathBuf>, PathBuf, PathBuf, PathBuf) {
+    let api = Api::new().unwrap();
+
+    let repo = api.model(model.to_string());
+
+    // Get the model.safetensor file(s)
+    let m = if let Ok(index) = repo.get("model.safetensors.index.json") {
+        let index = std::fs::File::open(index).unwrap();
+        let json: serde_json::Value = serde_json::from_reader(&index).unwrap();
+        let mut set = std::collections::HashSet::new();
+        if let Some(weight_map) = json.get("weight_map").unwrap().as_object() {
+            for v in weight_map.values() {
+                set.insert(v.as_str().unwrap().to_string());
+            }
+        }
+        set.iter().map(|p| repo.get(p).unwrap()).collect()
+    } else {
+        vec![repo.get("model.safetensors").unwrap()]
+    };
+
+    let c = repo.get("config.json").unwrap();
+    let t = repo.get("tokenizer.json").unwrap();
+    let tc = repo.get("tokenizer_config.json").unwrap();
+
+    (m, c, t, tc)
 }
