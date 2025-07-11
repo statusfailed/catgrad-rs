c6658262a0daab2c5a6930e383666b2bbd8dfec6
Jani Monoses <jani.monoses@gmail.com>
2025-07-11 10:29:04 +0300

    chat template in the Python script

--- catgrad-llm/examples/llm/llm.py
+++ catgrad-llm/examples/llm/llm.py
@@ -11,17 +11,31 @@ if __name__ == "__main__":
     )
     parser.add_argument("-p", "--prompt", type=str, default="Hello world")
     parser.add_argument("-s", "--seq-len", type=int, default=10)
+    parser.add_argument("-t", "--thinking", action="store_true")
     args = parser.parse_args()
 
     tokenizer = AutoTokenizer.from_pretrained(args.model)
     model = AutoModelForCausalLM.from_pretrained(args.model)
+
+    prompt = args.prompt
+
+    if tokenizer.chat_template is not None:
+        chat = [
+            {"role": "user", "content": prompt},
+        ]
+        prompt = tokenizer.apply_chat_template(
+            chat,
+            tokenize=False,
+            add_generation_prompt=True,
+            enable_thinking=args.thinking,
+        )
 
     # Remove model settings so generate does not warn about sampling parameters
     model.generation_config.temperature = None
     model.generation_config.top_p = None
     model.generation_config.top_k = None
 
-    inputs = tokenizer(args.prompt, return_tensors="pt")
+    inputs = tokenizer(prompt, return_tensors="pt")
     logits = model.generate(**inputs, max_new_tokens=args.seq_len, do_sample=False)
     output = tokenizer.decode(logits[0])
 
