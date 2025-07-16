e3e10cff242f209bae1a6e86779a566e4ddf408b
statusfailed <paul@statusfailed.com>
2025-07-16 14:38:34 +0100

    sketch core & shape categories, first tests

--- catgrad-core/src/util.rs
+++ catgrad-core/src/util.rs
@@ -2,17 +2,17 @@ use open_hypergraphs::lax::{OpenHypergraph, var::*};
 use std::cell::RefCell;
 use std::rc::Rc;
 
-pub fn build_typed<const Arity: usize, F, O: Clone, A: HasVar + Clone>(
-    source_types: [O; Arity],
+pub fn build_typed<const ARITY: usize, F, O: Clone, A: HasVar + Clone>(
+    source_types: [O; ARITY],
     f: F,
 ) -> BuildResult<O, A>
 where
-    F: Fn(&Rc<RefCell<OpenHypergraph<O, A>>>, [Var<O, A>; Arity]) -> Vec<Var<O, A>>,
+    F: Fn(&Rc<RefCell<OpenHypergraph<O, A>>>, [Var<O, A>; ARITY]) -> Vec<Var<O, A>>,
 {
     use std::array;
     build(move |state| {
         // use from_fn to avoid having to clone source_types
-        let sources: [Var<_, _>; Arity] =
+        let sources: [Var<_, _>; ARITY] =
             array::from_fn(|i| Var::new(state.clone(), source_types[i].clone()));
 
         let sources_vec = sources.iter().cloned().collect();
