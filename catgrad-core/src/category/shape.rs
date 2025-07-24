028b7e62dddf9f1c76857177bb8637683519f04e
statusfailed <paul@statusfailed.com>
2025-07-24 14:19:10 +0100

    WIP: shape checker

--- catgrad-core/src/category/shape.rs
+++ catgrad-core/src/category/shape.rs
@@ -4,47 +4,46 @@
 use super::core;
 use open_hypergraphs::lax::{OpenHypergraph, var};
 
+pub use super::core::{Dtype, TensorOp};
+
 /// Objects of the category.
 /// Note that Nat and Rank-1 shapes are only isomorphic so we can safely index by naturals.
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Object {
-    Nat,   // natural numbers
-    Shape, // tuples of natural numbers (TODO: dtype)
+    Nat,         // natural numbers
+    NdArrayType, // tuples of natural numbers (TODO: dtype)
     Tensor,
 }
 
 /// Operations are those of core, extended with operations on shapes
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Operation {
-    Shape(ShapeOp),
+    Type(TypeOp),
+    Nat(NatOp),
     Tensor(core::TensorOp),
     Copy,
 }
 
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum NatOp {
+    Constant(usize),
+
+    // Multiply
+    Mul,
+
+    /// Lift a natural number to a rank-1 shape and dtype
+    /// `Lift : Nat → Type`
+    Lift(Dtype),
+}
+
 /// Operations involving shapes
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub enum ShapeOp {
-    /// Concatenate two shapes
+pub enum TypeOp {
+    /// Concatenate n shapes
     Concat,
 
     /// Split a shape into a fixed number of nats
     Split,
-
-    /// Lift a natural number to a rank-1 shape
-    /// `Lift : Nat → Shape`
-    Lift,
-
-    // TODO: *semiring* ops on nats?
-    /// Multiply two nats
-    Mul,
-
-    // TODO: docs
-    // `Tensor → Type × Tensor` and its inverse
-    Annotate,
-    Coannotate,
-
-    // Syntactic equality(?)
-    Equal,
 }
 
 // Copy lets us use HasVar
@@ -64,66 +63,6 @@ pub type Builder = Rc<RefCell<Term>>;
 ////////////////////////////////////////////////////////////////////////////////
 // Helpers
 
-// [x, t] → [x : t]
-#[allow(dead_code)]
-fn annotate(builder: &Builder, x: Var, t: Var) -> Var {
-    assert_eq!(x.label, Object::Tensor);
-    assert_eq!(x.label, Object::Shape); // TODO: should be NdArrayType, not just shape
-
-    var::fn_operation(
-        builder,
-        &[x, t],
-        Object::Tensor,
-        Operation::Shape(ShapeOp::Annotate),
-    )
-}
-
-// (x : t) → [x, t]
-#[allow(dead_code)]
-fn coannotate(builder: &Builder, x: Var) -> (Var, Var) {
-    assert_eq!(x.label, Object::Tensor);
-
-    let result = var::operation(
-        builder,
-        &[x],
-        vec![Object::Tensor, Object::Shape],
-        Operation::Shape(ShapeOp::Coannotate),
-    );
-
-    assert_eq!(result.len(), 2);
-    (result[0].clone(), result[1].clone())
-}
-
-#[allow(dead_code)]
-fn split<const N: usize>(builder: &Builder, x: Var) -> [Var; N] {
-    assert_eq!(x.label, Object::Shape);
-
-    let result = var::operation(
-        builder,
-        &[x],
-        vec![Object::Shape; N],
-        Operation::Shape(ShapeOp::Coannotate),
-    );
-
-    result.try_into().expect("unexpected size!")
-}
-
-#[allow(dead_code)]
-fn concat<const N: usize>(builder: &Builder, xs: [Var; N]) -> Var {
-    // should all be *shapes*.
-    // TODO: if a nat, auto-lift to shape using Lift?
-    for x in &xs {
-        assert_eq!(x.label, Object::Shape);
-    }
-
-    var::fn_operation(
-        builder,
-        &xs,
-        Object::Shape,
-        Operation::Shape(ShapeOp::Coannotate),
-    )
-}
-
 /// Batch matmul
 pub fn matmul(builder: &Builder, f: Var, g: Var) -> Var {
     // checked during shapechecking, but errors easier to follow here.
@@ -138,7 +77,7 @@ pub fn matmul(builder: &Builder, f: Var, g: Var) -> Var {
     )
 }
 
-// reshape (t : Shape) (x : s) (s ≅ t) : (y : t)
+// reshape (t : Type) (x : s) (s ≅ t) : (y : t)
 pub fn reshape(builder: &Builder, t: Var, x: Var) -> Var {
     var::fn_operation(
         builder,
