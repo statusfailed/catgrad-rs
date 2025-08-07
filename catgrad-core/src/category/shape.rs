3b39b18e12fe0d0ae3eba24ac51aaed5173d7fa9
statusfailed <paul@statusfailed.com>
2025-08-07 16:29:44 +0100

    remove dead code

--- catgrad-core/src/category/shape.rs
+++ catgrad-core/src/category/shape.rs
@@ -2,7 +2,7 @@
 //! Extends [`core`] with shape operations.
 
 use super::core;
-use open_hypergraphs::lax::{OpenHypergraph, var};
+use open_hypergraphs::lax::var;
 
 pub use super::core::{Dtype, TensorOp};
 
@@ -72,97 +72,4 @@ impl var::HasMul<Object, Operation> for Operation {
             obj => panic!("no Mul operator for Object {obj:?}"),
         }
     }
-}
-
-pub type Var = var::Var<Object, Operation>;
-pub type Term = OpenHypergraph<Object, Operation>;
-
-use std::cell::RefCell;
-use std::rc::Rc;
-pub type Builder = Rc<RefCell<Term>>;
-
-////////////////////////////////////////////////////////////////////////////////
-// Type Helpers
-
-/// Unpack a shape into a dtype and its constituent Nat dimensions
-pub fn unpack<const N: usize>(builder: &Builder, x: Var) -> (Var, [Var; N]) {
-    assert_eq!(x.label, Object::NdArrayType);
-
-    let mut ty = vec![Object::Nat; N + 1];
-    ty[0] = Object::Dtype;
-
-    let elements = var::operation(builder, &[x], ty, Operation::Type(TypeOp::Unpack));
-
-    let mut iter = elements.into_iter();
-    let head = iter.next().unwrap();
-    let tail: [Var; N] = iter_to_array(iter).expect("N elements");
-    (head, tail)
-}
-
-/// Pack a fixed number of Nat values into a specific shape
-pub fn pack<const N: usize>(builder: &Builder, dtype: Var, xs: [Var; N]) -> Var {
-    // should all be *shapes*.
-    // TODO: if a nat, auto-lift to shape using Lift?
-    assert_eq!(dtype.label, Object::Dtype);
-
-    for x in &xs {
-        assert_eq!(x.label, Object::Nat);
-    }
-
-    let args: Vec<Var> = std::iter::once(dtype).chain(xs).collect();
-    var::fn_operation(
-        builder,
-        &args,
-        Object::NdArrayType,
-        Operation::Type(TypeOp::Pack),
-    )
-}
-
-fn iter_to_array<T, const N: usize>(mut iter: impl Iterator<Item = T>) -> Option<[T; N]> {
-    let mut vec = Vec::with_capacity(N);
-    for _ in 0..N {
-        vec.push(iter.next()?);
-    }
-    vec.try_into().ok()
-}
-
-// Tensor → NdArrayType
-pub fn shape(builder: &Builder, x: Var) -> Var {
-    var::fn_operation(
-        builder,
-        &[x],
-        Object::NdArrayType,
-        Operation::Type(TypeOp::Shape),
-    )
-}
-
-pub fn dtype_constant(builder: &Builder, dtype: Dtype) -> Var {
-    var::fn_operation(builder, &[], Object::Dtype, Operation::DtypeConstant(dtype))
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// Tensor Helpers
-
-/// Batch matmul
-pub fn matmul(builder: &Builder, f: Var, g: Var) -> Var {
-    // checked during shapechecking, but errors easier to follow here.
-    assert_eq!(f.label, Object::Tensor);
-    assert_eq!(g.label, Object::Tensor);
-
-    var::fn_operation(
-        builder,
-        &[f, g],
-        Object::Tensor,
-        Operation::Tensor(core::TensorOp::MatMul),
-    )
-}
-
-// reshape (t : Type) (x : s) (s ≅ t) : (y : t)
-pub fn reshape(builder: &Builder, t: Var, x: Var) -> Var {
-    var::fn_operation(
-        builder,
-        &[t, x],
-        Object::Tensor,
-        Operation::Tensor(core::TensorOp::Reshape),
-    )
 }
