187ee46ae781afc334151e549f406a419bb9cf83
georgewhewell <georgerw@gmail.com>
2025-08-16 18:57:01 +0200

    feat: adds LLMError

---
+++ catgrad-llm/src/error.rs
@@ -0,0 +1,24 @@
+use thiserror;
+
+#[derive(Debug, thiserror::Error)]
+pub enum LLMError {
+    #[error("IO error occurred: {0}")]
+    IoError(#[from] std::io::Error),
+
+    #[error("Unsupported tensor dtype: {0}")]
+    UnsupportedDtype(String),
+
+    #[error("Unsupported model architecture: {0}")]
+    UnsupportedModel(String),
+
+    #[error("Failed to parse JSON: {0}")]
+    JsonError(#[from] serde_json::Error),
+
+    #[error("Failed to deserialize safetensors: {0}")]
+    SafetensorsError(#[from] safetensors::SafeTensorError),
+
+    #[error("Tokenizer Error: {0}")]
+    TokenizerError(#[from] tokenizers::tokenizer::Error),
+    // #[error("Failed to access Hugging Face Hub: {0}")]
+    // HfHubError(String),
+}
--- catgrad-llm/src/lib.rs
+++ catgrad-llm/src/lib.rs
@@ -1,6 +1,10 @@
 //! LLM-specific code like tokenization and kv-cache logic which (currently) has to live outside
 //! the model graph.
+mod error;
 pub mod models;
 pub mod run;
 pub mod serve;
 pub mod utils;
+
+pub use error::LLMError;
+pub type Result<T, E = error::LLMError> = std::result::Result<T, E>;
