03faf465c469809327cb97310573056c495df1c0
statusfailed <paul@statusfailed.com>
2025-10-22 22:46:58 +0100

    add `lower` module with skeleton MLIR lowering

--- catgrad-mlir/src/lib.rs
+++ catgrad-mlir/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod grammar;
+pub mod lower;
 pub mod pass;
--- catgrad-mlir/src/pass.rs
+++ catgrad-mlir/src/pass.rs
@@ -1,19 +1,19 @@
-use catgrad::category::lang;
 use catgrad::prelude::*;
-use open_hypergraphs::lax::OpenHypergraph;
 
-// TODO
-#[derive(Debug)]
-pub struct MLIR;
+use super::grammar;
+use super::lower;
 
 // Render a TypedTerm and its associated environment of definitions to MLIR
-pub fn lang_to_mlir(env: &Environment, params: &typecheck::Parameters, term: TypedTerm) -> MLIR {
+pub fn lang_to_mlir(
+    env: &Environment,
+    params: &typecheck::Parameters,
+    term: TypedTerm,
+) -> Vec<grammar::Func> {
     // Typecheck `term` and get an open hypergraph annotated with types
     let node_annotations = typecheck::check(env, params, term.clone()).unwrap();
     let checked_term = term.term.clone().with_nodes(|_| node_annotations).unwrap();
 
-    // Produce MLIR from SSA, ignore dependencies
-    let entry_fragment = checked_term_to_mlir(checked_term);
+    let mlir = lower::term_to_func("term", &checked_term);
 
     // TODO: Produce MLIR for each used dependency: a list of MLIR fragments
     // let _definitions = todo!(); // list of MLIR strings / structs
@@ -23,9 +23,5 @@ pub fn lang_to_mlir(env: &Environment, params: &typecheck::Parameters, term: Typ
     //  - each used dependency
     //let result = todo!("concat _entry_fragment and _definitions");
 
-    entry_fragment
-}
-
-fn checked_term_to_mlir(_term: OpenHypergraph<typecheck::Type, lang::Operation>) -> MLIR {
-    todo!()
+    vec![mlir]
 }
