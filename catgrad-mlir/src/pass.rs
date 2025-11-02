6a3fe30d2a360494652d589bdf5315f39b132e61
statusfailed <paul@statusfailed.com>
2025-11-02 16:14:06 +0000

    check top-level term is dtype-monomorphic

--- catgrad-mlir/src/pass.rs
+++ catgrad-mlir/src/pass.rs
@@ -1,7 +1,21 @@
+use catgrad::category::lang;
 use catgrad::prelude::*;
+use open_hypergraphs::lax::*;
 
 use super::grammar;
 use super::lower;
+
+/// Ensure the boundaries of the entry point term do not have free Dtype vars
+fn is_dtype_monomorphic(ty: &Type) -> bool {
+    match &ty {
+        Type::Dtype(d) => match d {
+            typecheck::DtypeExpr::Constant(_) => true,
+            // Non-constant dtypes not permitted
+            _ => false,
+        },
+        _ => true,
+    }
+}
 
 // Render a TypedTerm and its associated environment of definitions to MLIR
 pub fn lang_to_mlir(
@@ -9,15 +23,21 @@ pub fn lang_to_mlir(
     params: &typecheck::Parameters,
     mut typed_term: TypedTerm,
 ) -> Vec<grammar::Func> {
+    // Verify no free Dtype variables (which cannot be compiled in MLIR)
+
     // Forget extraneous Copy operations
     typed_term.term = open_hypergraphs::lax::var::forget::forget_monogamous(&typed_term.term);
 
     // Typecheck `term` and get an open hypergraph annotated with *normalized* types
-    let node_annotations = typecheck::check(env, params, typed_term.clone())
+    let node_annotations: Vec<_> = typecheck::check(env, params, typed_term.clone())
         .unwrap()
         .into_iter()
         .map(typecheck::normalize)
         .collect();
+
+    // Verify there were no non-constant Dtypes in the entrypoint term's node annotations
+    // TODO: replace assertion with a Result
+    assert!(node_annotations.iter().all(is_dtype_monomorphic));
 
     // Create a term with normalized types for node labels
     let checked_term = typed_term
