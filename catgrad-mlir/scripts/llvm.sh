3d073ef51865c561a6ee69465d771655db61250a
statusfailed <paul@statusfailed.com>
2025-11-06 22:53:54 +0000

    add mlir debug scripts

---
+++ catgrad-mlir/scripts/llvm.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Lower all the way to LLVM
+mlir-opt $1 \
+  --convert-elementwise-to-linalg \
+  --linalg-fuse-elementwise-ops \
+  --one-shot-bufferize=bufferize-function-boundaries \
+  --convert-linalg-to-loops \
+  --convert-scf-to-cf \
+  --expand-strided-metadata \
+  --lower-affine \
+  --finalize-memref-to-llvm \
+  --convert-arith-to-llvm \
+  --convert-func-to-llvm \
+  --convert-cf-to-llvm \
+  --reconcile-unrealized-casts
