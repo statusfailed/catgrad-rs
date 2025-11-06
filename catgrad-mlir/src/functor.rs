d3e993f41994c61690f0cdb77b83cc833ca83d72
statusfailed <paul@statusfailed.com>
2025-11-06 22:56:42 +0000

    add functors to inline and forget id casts

---
+++ catgrad-mlir/src/functor.rs
@@ -0,0 +1,48 @@
+use open_hypergraphs::lax::functor::*;
+use open_hypergraphs::lax::*;
+
+use catgrad::category::lang::Operation;
+use catgrad::prelude::*;
+
+pub fn forget_identity_casts(
+    f: &OpenHypergraph<Type, Operation>,
+) -> OpenHypergraph<Type, Operation> {
+    ForgetIdentityCasts.map_arrow(f)
+}
+
+// not public: no use for this except via forget_monogamous
+#[derive(Clone)]
+struct ForgetIdentityCasts;
+
+fn proj0(t0: &Type, t1: &Type) -> OpenHypergraph<Type, Operation> {
+    let id = OpenHypergraph::identity(vec![t0.clone()]);
+    let copy = Path::new(vec!["cartesian", "copy"]).unwrap();
+    let discard = OpenHypergraph::singleton(Operation::Declaration(copy), vec![t1.clone()], vec![]);
+    &id | &discard
+}
+
+impl Functor<Type, Operation, Type, Operation> for ForgetIdentityCasts {
+    // Identity-on-objects
+    fn map_object(&self, o: &Type) -> impl ExactSizeIterator<Item = Type> {
+        std::iter::once(o.clone())
+    }
+
+    fn map_operation(
+        &self,
+        a: &Operation,
+        source: &[Type],
+        target: &[Type],
+    ) -> OpenHypergraph<Type, Operation> {
+        let cast = Path::new(vec!["tensor", "cast"]).unwrap();
+
+        if Operation::Declaration(cast) != *a {
+            return OpenHypergraph::singleton(a.clone(), source.to_vec(), target.to_vec());
+        }
+
+        proj0(&source[0], &source[1])
+    }
+
+    fn map_arrow(&self, f: &OpenHypergraph<Type, Operation>) -> OpenHypergraph<Type, Operation> {
+        define_map_arrow(self, f)
+    }
+}
--- catgrad-mlir/src/lib.rs
+++ catgrad-mlir/src/lib.rs
@@ -5,3 +5,8 @@ pub mod pass;
 // rendering Declarations
 pub mod ops;
 pub mod util;
+
+pub mod functor;
+
+// TODO: replace with catgrad::definition::inline
+pub mod inline;
