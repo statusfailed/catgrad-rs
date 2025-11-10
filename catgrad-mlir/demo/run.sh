92f8beb12d5414a05eb53889a55c6f04d9fe76ce
statusfailed <paul@statusfailed.com>
2025-11-10 20:11:33 +0000

    MLIR: update demo to show multiple returns

--- catgrad-mlir/demo/run.sh
+++ catgrad-mlir/demo/run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# compile helper
+gcc -o main main.c
+
 ../scripts/llvm.sh main.mlir > lowered.mlir
 mlir-translate lowered.mlir --mlir-to-llvmir -o main.ll
 llc -filetype=obj main.ll -o main.o
