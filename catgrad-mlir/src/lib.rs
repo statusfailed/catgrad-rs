7a12ff7a60c8be98776961ddf011090c32d31037
statusfailed <paul@statusfailed.com>
2025-11-12 13:13:01 +0000

    MLIR: add some docs

--- catgrad-mlir/src/lib.rs
+++ catgrad-mlir/src/lib.rs
@@ -1,7 +1,8 @@
-/// Lower [`catgrad::TypedTerm`]s to MLIR text
+//! # Catgrad MLIR backend
+
 pub mod lower;
 
-/// Transform MLIR text into a .so
+/// Transform MLIR text into a shared object file
 pub mod codegen;
 
 /// Execution of compiled shared objects
