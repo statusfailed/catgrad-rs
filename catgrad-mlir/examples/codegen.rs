908a9675967cab5b98e2cace4a3825e3855a1b40
statusfailed <paul@statusfailed.com>
2025-11-12 12:31:17 +0000

    MLIR: appease cargo fmt

--- catgrad-mlir/examples/codegen.rs
+++ catgrad-mlir/examples/codegen.rs
@@ -6,15 +6,15 @@ use catgrad_mlir::codegen::codegen;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
+
     if args.len() != 3 {
         eprintln!("Usage: {} <input.mlir> <output.so>", args[0]);
         process::exit(1);
     }
-    
+
     let mlir_file = &args[1];
     let output_so = &args[2];
-    
+
     // Read MLIR text from file
     let mlir_text = match fs::read_to_string(mlir_file) {
         Ok(content) => content,
@@ -23,7 +23,7 @@ fn main() {
             process::exit(1);
         }
     };
-    
+
     // Compile to shared library
     match codegen(&mlir_text, output_so) {
         Ok(()) => {
